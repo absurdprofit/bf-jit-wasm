0035300b1691e49d1f5bbfedd4fc4976516de3ac
Nathan Handmade <absurdprofit@icloud.com>
2026-05-09 09:07:01 -0500

    Implementing collect_tokens function

--- src/instruction.rs
+++ src/instruction.rs
@@ -2,52 +2,106 @@ use enum_dispatch::enum_dispatch;
 
 use crate::program::Program;
 
-pub struct Right;
-pub struct Left;
-pub struct Increment;
-pub struct Decrement;
-pub struct Input;
-pub struct Output;
-pub struct RightJump;
-pub struct LeftJump;
+pub struct Right {
+    count: usize,
+}
+pub struct Left {
+    count: usize,
+}
+pub struct Increment {
+    amount: u8,
+}
+pub struct Decrement {
+    amount: u8,
+}
+pub struct Input {
+    count: usize,
+}
+pub struct Output {
+    count: usize,
+}
+pub struct RightJump {
+    end: usize,
+}
+pub struct LeftJump {
+    start: usize,
+}
 
 #[enum_dispatch]
 pub trait Instruction {
     fn execute(&self, program: &mut Program) -> ();
 }
 
+impl Increment {
+    pub fn new(amount: u8) -> Self {
+        Self { amount }
+    }
+}
+
 impl Instruction for Increment {
     fn execute(&self, program: &mut Program) -> () {
-        program.memory[program.pointer] += 1;
+        let value = program.memory[program.pointer];
+        program.memory[program.pointer].wrapping_add(value + self.amount);
         program.counter += 1;
+    }
+}
+
+impl Decrement {
+    pub fn new(amount: u8) -> Self {
+        Self { amount }
     }
 }
 
 impl Instruction for Decrement {
     fn execute(&self, program: &mut Program) -> () {
-        program.memory[program.pointer] -= 1;
+        let value = program.memory[program.pointer];
+        program.memory[program.pointer].wrapping_sub(value + self.amount);
         program.counter += 1;
+    }
+}
+
+impl Left {
+    pub fn new(count: usize) -> Self {
+        Self { count }
     }
 }
 
 impl Instruction for Left {
     fn execute(&self, program: &mut Program) -> () {
         assert!(program.pointer > 0, "RUNTIME ERROR: Memory underflow.");
-        program.pointer -= 1;
+        program.pointer -= self.count;
         program.counter += 1;
+    }
+}
+
+impl Right {
+    pub fn new(count: usize) -> Self {
+        Self { count }
     }
 }
 
 impl Instruction for Right {
     fn execute(&self, program: &mut Program) -> () {
         // need to assert no overflow
-        program.pointer += 1;
+        program.pointer += self.count;
         program.counter += 1;
+    }
+}
+
+impl Input {
+    pub fn new(count: usize) -> Self {
+        Self { count }
     }
 }
 
 impl Instruction for Input {
     fn execute(&self, program: &mut Program) -> () {}
+}
+
+impl Output {
+    pub fn new(count: usize) -> Self {
+        Self { count }
+    }
 }
 
 impl Instruction for Output {
@@ -57,12 +111,36 @@ impl Instruction for Output {
     }
 }
 
+impl RightJump {
+    pub fn new(end: usize) -> Self {
+        Self { end }
+    }
+}
+
 impl Instruction for RightJump {
-    fn execute(&self, program: &mut Program) -> () {}
+    fn execute(&self, program: &mut Program) -> () {
+        program.counter = if program.memory[program.pointer] == 0 {
+            self.end + 1
+        } else {
+            program.counter + 1
+        }
+    }
+}
+
+impl LeftJump {
+    pub fn new(start: usize) -> Self {
+        Self { start }
+    }
 }
 
 impl Instruction for LeftJump {
-    fn execute(&self, program: &mut Program) -> () {}
+    fn execute(&self, program: &mut Program) -> () {
+        program.counter = if program.memory[program.pointer] != 0 {
+            self.start + 1
+        } else {
+            program.counter + 1
+        }
+    }
 }
 
 #[enum_dispatch(Instruction)]
--- src/program.rs
+++ src/program.rs
@@ -1,6 +1,6 @@
 use crate::{
-    instruction::{self, Instruction, InstructionSet},
-    tokeniser::Token,
+    instruction::{self, Instruction, InstructionSet, LeftJump, RightJump},
+    tokeniser::{self, Token},
 };
 
 pub struct Program {
@@ -20,14 +20,43 @@ impl Program {
         }
     }
 
-    pub fn run(&mut self) {
-        while self.counter < self.instructions.len() {
-            let instruction = &self.instructions[self.counter];
-            instruction.execute(self);
-        }
-    }
+    // pub fn run(&mut self) {
+    //     while self.counter < self.instructions.len() {
+    //         let instruction = &self.instructions[self.counter];
+    //         instruction.execute(self);
+    //     }
+    // }
 
     fn collect_tokens(tokens: impl Iterator<Item = Token>) -> Vec<InstructionSet> {
-        vec![]
+        let mut instructions: Vec<InstructionSet> = vec![];
+        let mut stack = vec![];
+        let mut token_instance: Option<Token> = None;
+        let mut token_instance_count = 0;
+        for token in tokens {
+            match token {
+                tokeniser::Token::RightJump => {
+                    stack.push(instructions.len());
+                    instructions.push(instruction::RightJump::new(0).into());
+                }
+                tokeniser::Token::LeftJump => {
+                    let start = stack.pop();
+                    assert!(start.is_some(), "SYNTAX ERROR: Unbalanced jump.");
+                    let start = start.unwrap();
+                    instructions[start] = instruction::RightJump::new(instructions.len()).into();
+                    instructions.push(instruction::LeftJump::new(start).into());
+                }
+                token => {
+                    if let Some(instance) = token_instance {
+                        if instance == token {
+                            token_instance_count += 1;
+                        } else {
+                            token_instance_count = 1;
+                            token_instance = Some(token);
+                        }
+                    }
+                }
+            }
+        }
+        instructions
     }
 }
