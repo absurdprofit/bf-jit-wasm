8582d5e728ff5a59ad6eb119bf3af2fdbab67539
Nathan Handmade <absurdprofit@icloud.com>
2026-05-10 16:17:36 -0500

    Implement necessary compiler error handling and value conversions for extern_compile.

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,27 +1,53 @@
 use wasm_bindgen::prelude::*;
 
-pub enum CompilerError {}
+pub enum CompilerError {
+    TypeError,
+    CompileError,
+    LinkError,
+    RuntimeError,
+    Defect,
+}
 
 pub struct RuntimeCompiler;
 
 pub trait Compiler {
     // TODO: figure out if we what the type of a WASM instance JS object would be.
-    fn compile(/* source: impl Iterator<Item = u8> */) -> Result<(), CompilerError>;
+    fn compile(source: impl Iterator<Item = u8>) -> Result<(), CompilerError>;
 }
 
 #[wasm_bindgen(module = "imports.js")]
 extern "C" {
     // TODO: define a streaming compiler interface
-    fn extern_compile() -> ();
+    // TODO: return a Ok(Future) that can be polled by the program.
+    #[wasm_bindgen(catch)]
+    fn extern_compile() -> Result<(), JsValue>;
 }
 
 #[cfg(target_arch = "wasm32")]
 impl Compiler for RuntimeCompiler {
     // source is not a full WASM binary, it is simply the concatenation of emit_wasm results from instructions.
-    fn compile(/*source: impl Iterator<Item = u8>*/) -> Result<(), CompilerError> {
-        extern_compile();
+    // compilation could failed, let's handle failures by matching the error ID.
+    // in the case of compilation failure we can simply do nothing and let the interpreter run to completion.
+    fn compile(source: impl Iterator<Item = u8>) -> Result<(), CompilerError> {
         // TODO: add module preamble
         // TODO: add program bytes
-        Ok(())
+        match extern_compile() {
+            Ok(result) => Ok(result),
+            Err(js_value) => {
+                let compiler_error = if let Some(error) = js_value.as_f64() {
+                    match error {
+                        0.0 => CompilerError::TypeError,
+                        1.0 => CompilerError::CompileError,
+                        2.0 => CompilerError::LinkError,
+                        3.0 => CompilerError::RuntimeError,
+                        _ => CompilerError::Defect,
+                    }
+                } else {
+                    CompilerError::Defect
+                };
+
+                Err(compiler_error)
+            }
+        }
     }
 }
--- src/web/imports.js
+++ src/web/imports.js
@@ -7,12 +7,51 @@ export function extern_write(byte) {
   console.log(String.fromCharCode(byte));
 }
 
+function createReadableFromIterable(iterable) {
+  const iterator = Symbol.iterator in iterable ? iterable[Symbol.iterator]() : iterable[Symbol.asyncIterator]();
+  return new ReadableStream({
+    async pull(controller) {
+      const { value, done } = await iterator.next();
+      if (done) {
+        controller.close();
+      }
+      else {
+        controller.enqueue(value);
+      }
+    },
+  });
+}
+
+
 let memory = new WebAssembly.Memory({
   initial: 0,
   maximum: 0
 });
-export function extern_compile() {
-  console.log(memory);
+export async function extern_compile(source_iterator) {
+  try {
+    const { instance } = await WebAssembly.instantiateStreaming(
+      new Response(createReadableFromIterable(source_iterator)),
+      {
+        env: {
+          memory
+        }
+      }
+    );
+    return instance;
+  } catch (e) {
+    switch (e.name) {
+      case "TypeError":
+        throw 0;
+      case "CompileError":
+        throw 1;
+      case "LinkError":
+        throw 2;
+      case "RuntimeError":
+        throw 3;
+      default:
+        throw -1;
+    }
+  }
 }
 
 export function setMemory(_memory) {
